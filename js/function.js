36a3f2bbe2de4147fb7320f10f62dafb842f5228
BenDoverMarij <115532706+MaBarel@users.noreply.github.com>
2023-01-18 12:34:23 +0100

    test 2

---
+++ js/function.js
@@ -0,0 +1,21 @@
+function init(){ 
+
+}
+function draw(){
+
+}
+function move(){
+
+}
+
+function game() {
+    draw()
+    move()
+}
+function gamestart(){
+    init()
+    document.addEventListener("keydown", function(e){})
+    interval = setInterval(game, 50)
+        
+
+}
--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
-const game = {};
-game.canvas = document.getElementById('canvas');
-game.ctx = game.canvas.getContext('2d');
-game.backgroundColor = '#000000';
-game.update = function() {
-  game.ctx.fillStyle = game.backgroundColor;
-  game.ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
-}
-game.keydown = function(e) {
-}      
-game.init = function() {
-  game.interval = setInterval(game.update, 1000 / 60);
-}
-game.stop = function() {
-  clearInterval(game.interval);
-}
-game.restart = function() {
-  game.stop();
-  game.init();
-}
-window.onload = game.init;
-window.onkeydown = game.keydown;
+const canvas = document.querySelector(".gameboard");
+const ctx = canvas.getContext("2d");
+const interval = {};
+window.onload = start
