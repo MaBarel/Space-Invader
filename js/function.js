bf093d58d52f8d77d6c595da1b883239298af5cc
BenDoverMarij <115532706+MaBarel@users.noreply.github.com>
2023-01-19 09:25:42 +0100

    Current Version non moving aliens/player

--- js/function.js
+++ js/function.js
@@ -1,10 +1,25 @@
-function init(){ 
-
+function GameObject(x, y, img) {
+    this.x = x; this.y = y; this.img = img; this.active = true;
 }
-function draw(){
-
+GameObject.prototype.draw = function (ctx) {
+    this.active && ctx.drawImage(this.img, this.x, this.y, 40, 40)
 }
-function move(){
+
+function init() {
+    let img = document.querySelector(".alien ")
+    for (let y = 0; y < 3; y++) {
+        for (var x = 0; x < 8; x++) {
+            invaders.push(new GameObject(50 + x * 50, 20 + y * 50, img))
+        }
+    }
+}
+function draw() {
+    ctx.fillstyle = "#ddd"
+    ctx.fillRect(0, 0, 0, 0)
+    invaders.forEach(inv => inv.draw(ctx))
+    player.draw(ctx)
+}
+function move() {
 
 }
 
@@ -12,10 +27,9 @@ function game() {
     draw()
     move()
 }
-function gamestart(){
+function gamestart() {
     init()
-    document.addEventListener("keydown", function(e){})
-    interval = setInterval(game, 50)
-        
-
+    document.addEventListener("keydown", function (e) { })
+    interval = setInterval(game, 50);
+    startMessage.classList.remove("show")
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const canvas = document.querySelector(".gameboard");
 const ctx = canvas.getContext("2d");
-const interval = {};
-window.onload = start
+const player = new GameObject(230, 550, document.querySelector(".playerblack"));
+const startButton = document.querySelector("#startbutton");
+const startMessage = document.querySelector(".startmessage");
+let interval = {};
+let invaders = [];
+startButton.addEventListener("click", gamestart)
